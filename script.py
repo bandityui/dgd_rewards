87280f62673ba380d08faf968a54b283bbf01cd0
thomas <northeyt@live.ie>
2018-09-11 20:35:16 +0100

    first commit

---
+++ script.py
@@ -0,0 +1,63 @@
+from variables import *
+
+# A calculation of the annual tx fee rewards gained by DGD DAO participators/voters
+# ================================================================
+# Start by print >> f,ing information
+f=open('output.txt','w')
+print >> f, 'Calculate DGD rewards...'
+print >> f, '\n'
+print >> f, 'Based on the following assumptions:' 
+print >> f, 'Total number of DGX = ' + str(n_dgx/1e6) + 'm' + ' (' + str(gold_usd*n_dgx/1e9) + 'b USD)'
+print >> f, 'Average daily DGX on-chain transactions = ' + str(daily_dgx_transacted/1e6) + 'm' + ' (' + str(gold_usd*daily_dgx_transacted/1e6) + 'm USD)'
+print >> f, 'Percent of DGD locked up to receive rewards = ' + str(ratio_locked_up*100)
+print >> f, 'Percent of locked up DGD actively voting = ' + str(ratio_voting*100)
+print >> f, '\n'
+# ================================================================
+
+
+# ================================================================
+# TRANSACTION FEE CALCULATION
+
+# Daily tx fees that go to the locked up and voting DGD
+daily_tx_fees = daily_dgx_transacted * tx_fee_ratio / ( ratio_locked_up * ratio_voting) 
+
+# Annual tx fees
+annual_tx_fees = daily_tx_fees * 360
+
+# Print answers in DGX and USD
+print >> f, 'Total transaction fees (DGX) earned per annum:'
+print >> f, annual_tx_fees
+print >> f, 'Total transaction fees (USD) earned per annum:'
+print >> f, annual_tx_fees * gold_usd
+print >> f, '\n'
+
+# Annual tx fees per DGD token
+annual_tx_fees_per_dgd = annual_tx_fees / n_dgd
+
+# Print answers in DGX and USD
+print >> f, 'Transaction fees (DGX) earned per DGD per annum:'
+print >> f, annual_tx_fees_per_dgd
+print >> f, 'Transaction fees (USD) earned per DGD per annum:'
+print >> f, annual_tx_fees_per_dgd * gold_usd
+print >> f, '\n'
+# ================================================================
+
+# ================================================================
+# DEMURRAGE
+annual_demurrage_per_dgd = ( n_dgx / n_dgd ) * demurrage_ratio * ACT / 360
+print >> f, 'Demurrage fees (DGX) earned per DGD per annum:'
+print >> f, annual_demurrage_per_dgd
+print >> f, 'Demurrage fees (USD) earned per DGD per annum:'
+print >> f, annual_demurrage_per_dgd * gold_usd
+print >> f, '\n'
+# ================================================================
+
+# ================================================================
+# DGD VALUATION CALCULATION
+# Assume 5% rewards per year is satisfactory (thus, 20 * tx_fees_per_dgd)
+print >> f, 'Rational valuation of DGD based on 5% rewards per year...' 
+print >> f, 'Expected DGD value (USD):'
+print >> f, ( annual_tx_fees_per_dgd + annual_demurrage_per_dgd ) * gold_usd * 20 
+# ================================================================
+
+f.close()
---
+++ variables.py
@@ -0,0 +1,20 @@
+
+# Variables
+
+# Estimated values
+ratio_locked_up = 0.5				# Ratio of DGD locked up for quarterly rewards
+ratio_voting = 1.0				# Ratio of locked up DGD that is active and voting
+n_dgx = 652000				# Total number of DGX tokens
+daily_dgx_transacted = 0.01 * n_dgx		# Total DGX tokens transacted on-chain per day
+#daily_dgx_transacted = 144e6		# Total DGX tokens transacted on-chain per day
+ACT = 40 					# Number of full days on average per year DGX is held (affects demurrage)
+
+# Semi-fixed values
+gold_usd = 45					# USD value of 1 g of gold ( = 1 DGX token)
+
+# Fixed values
+n_dgd = 2e6					# Total number of DGDs
+tx_fee_ratio = 0.0013				# Transaction fee (0.13%)
+demurrage_ratio = 0.002				# Demurrage fee that goes to DGD holders (0.2%)
+
+
